8827496f58b714a6d435c53484499c8d584bcc07
Lewis Wilson <lewiswilson1998@hotmail.co.uk>
2019-12-16 13:25:54 +0000

    Grid with nodes

---
+++ IWM Game Jam 2019/Assets/Scripts/Node.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    // Potential city object, mission object etc 
+    private Vector3 m_world_position;
+    private GameObject m_node; 
+
+    public Node(Vector3 _world_position)
+    {
+        m_world_position = _world_position; 
+    }
+
+    public void Start()
+    {
+        // Instantiate node prefab from resources folder to world position
+        m_node = Instantiate(Resources.Load("Node", typeof(GameObject)), m_world_position, Quaternion.identity, GameObject.Find("Nodes").transform) as GameObject; 
+    }
+
+    public Vector3 GetWorldPosition()
+    {
+        return m_world_position; 
+    }
+
+    public GameObject GetNodeObject()
+    {
+        return m_node; 
+    }
+}
---
+++ IWM Game Jam 2019/Assets/Scripts/WorldGrid.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGrid : MonoBehaviour
+{
+    [SerializeField] private Vector2 m_grid_size;
+    [SerializeField] private float node_radius;
+
+    private Node[,] m_grid;
+    private int grid_size_X, grid_size_Y;
+    private float node_diameter;
+
+    private void Start()
+    {
+        node_diameter = node_radius * 2;
+        grid_size_X = Mathf.RoundToInt(m_grid_size.x / node_diameter);
+        grid_size_Y = Mathf.RoundToInt(m_grid_size.y / node_diameter);
+        CreateGrid();
+    }
+
+    private void CreateGrid()
+    {
+        m_grid = new Node[grid_size_X, grid_size_Y];
+        Vector3 world_bottom_left = transform.position - Vector3.right * m_grid_size.x / 2 - Vector3.forward * m_grid_size.y / 2;
+
+        // Loop through X and Y coord
+        for (int x = 0; x < grid_size_X; x++)
+        {
+            for (int y = 0; y < grid_size_Y; y++)
+            {
+                Vector3 world_point = world_bottom_left + Vector3.right * (x * node_diameter + node_radius) + Vector3.forward * (y * node_diameter + node_radius);
+                m_grid[x, y] = new Node(world_point);
+            }
+        }
+
+        foreach (Node n in m_grid)
+        {
+            n.Start();
+            n.GetNodeObject().transform.localScale = Vector3.one * (node_diameter - 0.1f); 
+        }
+    }
+
+    /// Temp DrawGizmo for cubes 
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.DrawWireCube(transform.position, new Vector3(m_grid_size.x, 1, m_grid_size.y));
+
+    //    if (m_grid != null)
+    //    {
+    //        foreach (Node n in m_grid)
+    //        {
+    //            Gizmos.color = Color.blue;
+    //            Gizmos.DrawCube(n.GetWorldPosition(), Vector3.one * (node_diameter - 0.1f));
+    //        }
+    //    }
+    //}
+}
+
